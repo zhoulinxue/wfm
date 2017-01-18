80da02172b60e9f6046620ca0233a13d9012e976
zx <194093798@qq.com>
2017-01-18 18:00:14 +0800

    增加N系列 适配

---
+++ app/src/main/java/com/zx/wfm/ui/InitActivity.java
@@ -0,0 +1,18 @@
+package com.zx.wfm.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.zx.wfm.R;
+
+/**
+ * Created by Administrator on 2017/1/17.
+ */
+
+public class InitActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.init_layout);
+    }
+}
